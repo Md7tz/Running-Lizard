5e4d894acccc4aef51b853305145e7aa650cfaed
omar_essam1 <68665060+omaressamheagazy@users.noreply.github.com>
2021-12-27 11:46:28 +0400

    add food.cpp

---
+++ Position.cpp
@@ -0,0 +1,8 @@
+#include "Utilities/position.h"
+Position::Position()
+{
+    x = -50;
+    y = 0;
+}
+
+Position::~Position() {}
---
+++ food.cpp
@@ -0,0 +1,22 @@
+#ifndef FOOD_H
+#define FOOD_H
+
+#include <cstdint>
+#include <graphics.h>
+#include "../Utilities/position.h"
+
+class Food
+{
+protected:
+	uint8_t randInt;
+	int foodColor;
+
+public:
+	Position foodPos;
+	void draw() const;				 		 // Draw the food
+	void generate(int16_t, int16_t); 		 // Generate food position
+	virtual bool update(int16_t, int16_t);	 // Status of food
+
+};
+
+#endif
