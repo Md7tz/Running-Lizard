585cfa06516d23437b77c9e2abc1306ebb8aa92a
Medhat Usama <medhatusama@gmail.com>
2022-02-03 05:40:12 +0300

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,2 +1,4 @@
-@REM g++ app.cpp -o bin/production/"Running Lizard" -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32 -lwinmm  && bin\production\"Running Lizard"
-g++ src/*.cpp -o bin/production/"Running-Lizard" -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -loleaut32 -lole32 -lwinmm && bin\production\"Running-Lizard"
+@REM old config: g++ app.cpp -o bin/production/"Running Lizard" -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32 -lwinmm  && bin\production\"Running Lizard"
+@REM -mwindows flag hides console
+@REM open the exe file after building in the root folder to work
+g++ -std=c++17 -mwindows src/*.cpp -o bin/production/"Running-Lizard" -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -loleaut32 -lole32 -lwinmm && bin\production\"Running-Lizard"
